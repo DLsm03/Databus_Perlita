920618c46983475964f0079ce99caade3a05f9a2
DLsm03 <l201140050@surnayarit.tecnm.mx>
2022-03-22 18:22:58 -0700

    Agregue Formularios

---
+++ WindowsFormsApp1/Forms/frmCamion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interfaces.Forms
+{
+    public partial class frmCamion : Form
+    {
+        public frmCamion()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCamion_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WindowsFormsApp1/Forms/frmInicio.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Interfaces.Forms
+{
+    public partial class frmInicio : Form
+    {
+        public frmInicio()
+        {
+            InitializeComponent();
+        }
+
+        private void timerFechaHora_Tick(object sender, EventArgs e)
+        {
+            lblHora.Text = DateTime.Now.ToString("hh:mm:ss");
+            lblFecha.Text = DateTime.Now.ToLongDateString();
+        }
+    }
+}
